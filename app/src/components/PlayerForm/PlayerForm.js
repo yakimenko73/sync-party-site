ab0c103370f440d4ff779b96031e015e72bb7872
r.yakimenko <r.yakimenko@simcord.com>
2022-01-26 14:34:16 +0200

    forms: add video player form

---
+++ app/src/components/PlayerForm/PlayerForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './PlayerForm.css'
+
+export default function PlayerForm(props) {
+  return (
+    <div className='player-wrapper'>
+      <div className='player'>
+        <h1 className='no-content'>No content</h1>
+      </div>
+    </div>
+  )
+}
--- app/src/components/RoomForm/RoomForm.js
+++ app/src/components/RoomForm/RoomForm.js
@@ -1,6 +1,7 @@
 import React from "react";
 import HeaderForm from "../HeaderForm/HeaderForm";
 import ChatForm from "../ChatForm/ChatForm";
+import PlayerForm from "../PlayerForm/PlayerForm";
 
 export default function RoomForm() {
   return (
@@ -8,6 +9,7 @@ export default function RoomForm() {
       <HeaderForm/>
       <main className='main'>
         <ChatForm/>
+        <PlayerForm/>
       </main>
     </>
   )
