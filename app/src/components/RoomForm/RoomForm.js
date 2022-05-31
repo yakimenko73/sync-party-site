43017f3d8a291c04afb97f7ab69c4670321b5ffc
r.yakimenko <r.yakimenko.73@gmail.com>
2022-06-01 01:36:06 +0300

    ws: add codes handling

--- app/src/components/RoomForm/RoomForm.js
+++ app/src/components/RoomForm/RoomForm.js
@@ -1,20 +1,22 @@
 import React, {useCallback, useEffect, useMemo, useRef, useState} from "react";
+import axios from "axios";
 import HeaderForm from "../HeaderForm/HeaderForm";
 import ChatForm from "../ChatForm/ChatForm";
 import PlayerForm from "../PlayerForm/PlayerForm";
 import './RoomForm.css'
 import ChatMessageLineDto from "../../dto/ChatMessageLineDto";
 import {useLocation} from "react-router";
-import axios from "axios";
 import {useNavigate} from "react-router-dom";
 import RoomMemberDto from "../../dto/RoomMemberDto";
 import retrieveRoomKey from "../../utils/utils";
+import ErrorForm from "../common/error/ErrorForm/ErrorForm";
 
 export default function RoomForm() {
   const location = useLocation()
   const navigate = useNavigate()
   const [chatMessages, appendMessageLine] = useState([])
   const [roomMembers, appendMember] = useState([])
+  const [errorOccurred, setErrorOccurred] = useState(false)
   const ws = useRef(null)
   const room_key = useMemo(() => retrieveRoomKey(location.pathname), [location])
   const wsCommands = useMemo(() => ({
@@ -26,6 +28,16 @@ export default function RoomForm() {
       appendMember(prevState => prevState.filter(member => member.id !== command.id))
     }
   }), [roomMembers])
+  const wsCodes = useMemo(() => ({
+    1000: function (reason) {
+      setErrorOccurred(prevState => !prevState)
+    },
+    1001: function (_) {
+    },
+    1011: function (reason) {
+      console.error(reason)
+    }
+  }), [])
 
   const handleChatMessage = (message) => {
     let messageLine = new ChatMessageLineDto(message)
@@ -46,14 +58,17 @@ export default function RoomForm() {
         }))
       };
 
-      ws.current.onclose = () => {
-        console.debug("WS: Connection close")
+      ws.current.onclose = (event) => {
+        console.debug(`WS: Connection close`)
+        wsCodes[event.code](event.reason)
       };
 
       ws.current.onmessage = e => {
         let payload = JSON.parse(e.data).data
-        if (!payload)
+        if (!payload) {
           console.warn(`WS: Receive unexpected message ${JSON.stringify(payload)}`)
+          return
+        }
         let message = payload.message
         let command = payload.command
         if (message)
@@ -71,12 +86,13 @@ export default function RoomForm() {
       .then(res => {
         let members = res.data.map(member => new RoomMemberDto(member))
         appendMember(prevState => prevState.concat(members))
-
-        openWsConnection()
       })
       .catch(res => {
         if (res.response.status === 404)
           navigate('room-not-found')
+      })
+      .finally(() => {
+        openWsConnection()
       })
   }, [roomMembers])
 
@@ -89,13 +105,16 @@ export default function RoomForm() {
     }
   }, []);
 
-  return (
-    <>
-      <HeaderForm/>
-      <main className='main'>
-        <ChatForm ws={ws} messages={chatMessages}/>
-        <PlayerForm viewers={roomMembers.length}/>
-      </main>
-    </>
-  )
+  if (errorOccurred)
+    return <ErrorForm/>
+  else
+    return (
+      <>
+        <HeaderForm/>
+        <main className='main'>
+          <ChatForm ws={ws} messages={chatMessages}/>
+          <PlayerForm viewers={roomMembers.length}/>
+        </main>
+      </>
+    )
 }
---
+++ app/src/constants/ws/reasons.js
@@ -0,0 +1,4 @@
+import React from "react";
+
+export const USER_ALREADY_IN_ROOM = "UserAlreadyInRoom";
+export const COOKIE_SESSION_NOT_FOUND = "CookieDoesNotExist";
