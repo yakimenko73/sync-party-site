9f9869bf9b5a4a2ec1b8fe2563ffd6c122f97831
r.yakimenko <r.yakimenko.73@gmail.com>
2022-06-04 21:12:59 +0300

    components: fix ErrorForm styles

--- app/src/components/common/error/ErrorForm/ErrorForm.js
+++ app/src/components/common/error/ErrorForm/ErrorForm.js
@@ -1,17 +1,21 @@
 import React from "react";
 import {SiOdysee} from 'react-icons/si';
 import './ErrorForm.css'
+import HeaderForm from "../../../HeaderForm/HeaderForm";
 
 export default function ErrorForm(props) {
   return (
     <>
-      <div className="container">
-        <SiOdysee className={"astronaut"}/>
-        <div className={"description"}>
-          <h1>Houston we have a problem :/</h1>
-          <div>{props.description}</div>
+      <main>
+        <HeaderForm/>
+        <div className="error-container">
+          <SiOdysee className={"astronaut"}/>
+          <div className={"description"}>
+            <h1>Houston we have a problem :/</h1>
+            <div>Lol kek</div>
+          </div>
         </div>
-      </div>
+      </main>
     </>
   )
 }
